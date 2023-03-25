f4839e35247edc0b4bc17a231c117013a23b2912
Garvit Sharma <61928763+gtxPrime@users.noreply.github.com>
2023-03-25 14:06:48 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -385,7 +385,7 @@ async def get_shortlink(link):
         link = link.replace("http", https)
     url = f'https://omegalinks.in/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
-              'link': link,
+              'url': link,
               }
 
     try:
@@ -396,7 +396,7 @@ async def get_shortlink(link):
                     return data['shortenedUrl']
                 else:
                     logger.error(f"Error: {data['message']}")
-                    return f'https://{URL_SHORTENR_WEBSITE}/api?api={URL_SHORTNER_WEBSITE_API}&link={link}'
+                    return f'https://{URL_SHORTENR_WEBSITE}/api?api={URL_SHORTNER_WEBSITE_API}&url={link}'
 
     except Exception as e:
         logger.error(e)
