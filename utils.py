626bc47120e64942e6aa43b192b6ac8fd479a159
Garvit Sharma <61928763+gtxPrime@users.noreply.github.com>
2023-03-25 14:13:26 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -393,7 +393,7 @@ async def get_shortlink(link):
             async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
                 data = await response.json()
                 if data["status"] == "success":
-                    return data['shortenedUrl']
+                    return data["shortenedUrl"]
                 else:
                     logger.error(f"Error: {data['message']}")
                     return f'https://{URL_SHORTENR_WEBSITE}/api?api={URL_SHORTNER_WEBSITE_API}&url={link}'
